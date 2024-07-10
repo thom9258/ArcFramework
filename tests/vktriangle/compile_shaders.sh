8551838d89b0a615cc19c442422bd5a02a894078
Thomas Alexgaard <thomasjensen9258@gmail.com>
2024-07-10 13:00:50 +0200

    added a hello triangle test

---
+++ tests/vktriangle/compile_shaders.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+glslc triangle.vert -o triangle.vert.spv
+glslc triangle.frag -o triangle.frag.spv
---
+++ tests/vktriangle/main.cpp
@@ -0,0 +1,55 @@
+#include <arc/Context.hpp>
+
+#define WIDTH 1200
+#define HEIGHT 900
+
+int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
+    
+    const arc::GraphicsContext::ValidationLayers validation_layers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    const arc::GraphicsContext::DeviceExtensions device_extensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    auto context = arc::GraphicsContext::create(WIDTH,
+                                                HEIGHT,
+                                                validation_layers,
+                                                device_extensions);
+
+    bool exit = false;
+    SDL_Event event;
+    while (!exit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                exit = true;
+                break;
+
+            case SDL_KEYDOWN:
+                switch( event.key.keysym.sym ) {
+                case SDLK_ESCAPE:
+                    exit = true;
+                    break;
+                }
+                break;
+
+            case SDL_WINDOWEVENT: {
+                const SDL_WindowEvent& wev = event.window;
+                switch (wev.event) {
+                case SDL_WINDOWEVENT_RESIZED:
+                case SDL_WINDOWEVENT_SIZE_CHANGED:
+                    /*Not handled yet*/
+                    break;
+                case SDL_WINDOWEVENT_CLOSE:
+                    exit = true;
+                    break;
+                }
+            } break;
+            }
+        }
+    }
+}
