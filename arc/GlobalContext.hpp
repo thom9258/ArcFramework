ee4354d21b53aaf1d0762223461a2ec8474e6d45
Thomas Alexgaard <thomasjensen9258@gmail.com>
2024-08-24 20:17:52 +0200

    started commenting the main classes

--- arc/GlobalContext.hpp
+++ arc/GlobalContext.hpp
@@ -1,4 +1,10 @@
 #include "SDLVulkan.hpp"
+/*********************************************************************
+ * @file GlobalContext.cpp
+ * @brief The Global Vulkan and SDL Context manager.
+ * @copyright
+ * Copyright 2024 Thomas Alexgaard Jensen
+ *********************************************************************/
 
 #include "TypeTraits.hpp"
 
@@ -7,19 +13,35 @@
 
 namespace ArcGraphics {
     
-
-struct GlobalContextInstance {
-    GlobalContextInstance();
-    ~GlobalContextInstance();
-};
-    
-class GlobalContext : public IsNotLvalueCopyable, IsNotRvalueCopyable
+/**
+* @brief The globally-available context for the vulkan and SDL API's.
+* @note This class *must* be the first one created.
+*/
+class GlobalContext 
+    : public IsNotLvalueCopyable
+    , public IsNotRvalueCopyable
 {
+    /**
+    * @brief Construct the global context of Vulkan and SDL.
+    */
+    GlobalContext();
+
+    /**
+    * @brief Destroy the global context of Vulkan and SDL.
+    * @note Destruction will only happen once the process is stopped. 
+    */
+    ~GlobalContext();
 public:
+
+    /**
+    * @brief Initialize the singleton context.
+    * The initialization is handled internally by the Device class when it is created.
+    * @see ArcGraphics::Device
+    */
     static void Initialize();
-    static void DeInitialize();
-    static std::unique_ptr<GlobalContextInstance> instance;
+
+private:
+    static std::unique_ptr<GlobalContext> m_context_instance; /// The singleton instance
 };
-    
 
 } 
