2e0b54472b3a8daf87715a0f6710a98a616dc510
Thomas Alexgaard <thomasjensen9258@gmail.com>
2024-08-24 21:40:47 +0200

    added comments to device and half of algorithm

--- arc/Algorithm.hpp
+++ arc/Algorithm.hpp
@@ -1,4 +1,18 @@
 #pragma once
+/** *******************************************************************
+ * @file Algorithm.hpp
+ * @brief The grand collection of different Vulkan and SDL related tools.
+ *
+ * This file is designed to be the backbone of ArcGraphics, yet is
+ * also as modular as possible, to the extent where it can be used anywhere.
+ *
+ * The functions are designed to all be pure, to minimize the mental overload
+ * of each.
+ *
+ * @copyright
+ * Copyright 2024 Thomas Alexgaard Jensen
+ *********************************************************************/
+
 
 #include "SDLVulkan.hpp"
 #include "GLM.hpp"
@@ -10,24 +24,51 @@
 
 namespace ArcGraphics {
     
+/**
+ * @brief A collection of validation layers.
+ */
 using ValidationLayers = std::vector<const char*>;
+    
+/**
+ * @brief A collection of device extensions.
+ */
 using DeviceExtensions = std::vector<const char*>;
     
-using Score = uint32_t;
-using ScoredDevice = std::pair<Score, VkPhysicalDevice>;
+/**
+ * @brief The internal scoring value for physical device selection.
+ */
+using DeviceScore = uint32_t;
 
+/**
+ * @brief A device and its scoring value.
+ */
+using ScoredDevice = std::pair<DeviceScore, VkPhysicalDevice>;
 
+/**
+ * @brief Properties & Features associated with a device.
+ */
 struct PhysicalDevicePropertyFeatureSet {
     VkPhysicalDeviceProperties properties;
     VkPhysicalDeviceFeatures features;
 };
     
+/**
+ * @brief The indices of the relevant queue families of a physical device.
+ */
 struct QueueFamilyIndices {
-    std::optional<uint32_t> graphics;
-    std::optional<uint32_t> present;
+    std::optional<uint32_t> graphics; /// graphics index
+    std::optional<uint32_t> present;  /// presentation index
+
+    /**
+    * @brief Predicate for if all the required indices found.
+    */
     bool is_complete() {
         return graphics && present;
     }
+
+    /**
+    * @brief Stringifier for debugging.
+    */
     std::string stringify() {
         std::stringstream ss;
         if (graphics)
@@ -42,16 +83,33 @@ struct QueueFamilyIndices {
     }
 };
     
-   
-//std::tuple<VkSurfaceFormatKHR, VkSwapchainKHR, std::vector<VkImageView>>
+/**
+ * @brief Return value for creating swap chains.
+ */
 struct CreatedSwapChain {
     VkSwapchainKHR swap_chain;
     std::vector<VkImageView> image_views;
     VkSurfaceFormatKHR surface_format;
 };
 
+/**
+ * @brief Create swap chain.
+ * @throw std::runtime_error if creating swap chain is not possible.
+ */
+[[nodiscard]]
+CreatedSwapChain create_swap_chain(const VkPhysicalDevice physical_device,
+                                   const VkDevice logical_device,
+                                   const VkSurfaceKHR window_surface,
+                                   const uint32_t width,
+                                   const uint32_t height);
+
+/**
+ * @brief A collection of Rendering Capabilities for a physical device. 
+ * @note This is essencially a condensed version of VkSurfaceCapabilitiesKHR
+ * where everything non-constant is removed, such as things related to the
+ * current surface.
+ */
 struct DeviceRenderingCapabilities {
-    // A condensed collection of members from VkSurfaceCapabilitiesKHR
     uint32_t min_image_count;
     uint32_t max_image_count;
     uint32_t max_image_array_layers;
@@ -64,83 +122,146 @@ struct DeviceRenderingCapabilities {
     std::vector<VkPresentModeKHR> present_modes;
 };
 
+/**
+ * @brief Get Rendering Capabilities of a physical device and its surface.
+ */
+[[nodiscard]]
+DeviceRenderingCapabilities get_rendering_capabilities(const VkPhysicalDevice& device, 
+                                                       const VkSurfaceKHR& surface);
+
+/**
+ * @brief Create Application information for the Vulkan instance.
+ */
 [[nodiscard]]
 VkApplicationInfo create_app_info(const char* appname);
 
     
+/**
+ * @brief Get extensions for window & instance.
+ */
 [[nodiscard]]
 std::vector<const char*> get_available_extensions(SDL_Window* window);
 
    
+/**
+ * @brief Create descriptor pool sizes from the desired bindings and frames in flight.
+ */
 [[nodiscard]]
 std::vector<VkDescriptorPoolSize>
 create_descriptor_pool_sizes(const std::vector<VkDescriptorSetLayoutBinding> bindings,
                              const uint32_t frames_in_flight);
     
 
+/**
+ * @brief Create instance info with decired extensions.
+ */
 [[nodiscard]]
 VkInstanceCreateInfo create_instance_info(const std::vector<const char*>& extensions,
                                           const VkApplicationInfo* app_info);
 
+/**
+ * @brief Get extension properties.
+ */
 [[nodiscard]]
 std::vector<VkExtensionProperties> get_available_extension_properties();
 
+/**
+ * @brief Get available validation layers.
+ */
 [[nodiscard]]
 std::vector<VkLayerProperties> get_available_validation_layers();
     
+/**
+ * @brief Check that a collection of validation layers are available.
+ */
 [[nodiscard]]
 bool is_validation_layers_supported(const ValidationLayers layers);
     
+/**
+ * @brief Check that a collection of validation layers are available.
+ */
 [[nodiscard]]
 std::vector<VkPhysicalDevice> get_available_physical_devices(const VkInstance instance);
 
+/**
+ * @brief Get properties of physical device.
+ */
 [[nodiscard]]
 VkPhysicalDeviceProperties get_physical_device_properties(const VkPhysicalDevice device);
     
+/**
+ * @brief Get collection of memory properties of physical device.
+ */
 [[nodiscard]]
 VkPhysicalDeviceMemoryProperties
 get_physical_device_memory_properties(const VkPhysicalDevice device);
 
+/**
+ * @brief Find suitable memory type in collection of memory properties.
+ */
 [[nodiscard]]
 uint32_t find_memory_type(const VkPhysicalDeviceMemoryProperties mem_properties,
                           const uint32_t type_filter,
                           const VkMemoryPropertyFlags property_flags);
  
+/**
+ * @brief Get collection of physical device features.
+ */
 [[nodiscard]]
 VkPhysicalDeviceFeatures get_physical_device_features(const VkPhysicalDevice device);
 
+/**
+ * @brief Get both collections of of physical device properties & features.
+ */
 [[nodiscard]]
-PhysicalDevicePropertyFeatureSet get_physical_device_properties_features(const VkPhysicalDevice device);
+PhysicalDevicePropertyFeatureSet
+get_physical_device_properties_features(const VkPhysicalDevice device);
 
+/**
+ * @brief Get physical device indice families.
+ */
+[[nodiscard]]
+std::vector<VkQueueFamilyProperties> get_queue_families(const VkPhysicalDevice& device);
+
+/**
+ * @brief Find graphics & presentation indices in collection of indice families.
+ */
 [[nodiscard]]
 QueueFamilyIndices 
 find_graphics_present_indices(const std::vector<VkQueueFamilyProperties> families,
                               const VkPhysicalDevice& device,
                               const VkSurfaceKHR& surface);
 
-[[nodiscard]]
-std::vector<VkQueueFamilyProperties> get_queue_families(const VkPhysicalDevice& device);
-
+/**
+ * @brief Get physical device extensions.
+ */
 [[nodiscard]]
 std::vector<VkExtensionProperties> get_device_extensions(const VkPhysicalDevice& device);
 
-    
+/**
+* @brief check if physical device supports the specified extensions.
+*/
 [[nodiscard]]
 bool is_device_extensions_supported(const VkPhysicalDevice device,
                                     const std::vector<const char*> needed_extensions);
 
+/**
+* @brief check if physical device supports the specified extensions.
+*/
 [[nodiscard]]
 std::vector<VkSurfaceFormatKHR> get_swap_chain_formats(const VkPhysicalDevice& device,
                                                        const VkSurfaceKHR& surface);
 
+/**
+* @brief Get collection of presentation modes of a swap chain created from physical device.
+*/
 [[nodiscard]]
 std::vector<VkPresentModeKHR> get_swap_chain_present_modes(const VkPhysicalDevice& device,
                                                            const VkSurfaceKHR& surface);
 
-[[nodiscard]]
-DeviceRenderingCapabilities get_rendering_capabilities(const VkPhysicalDevice& device, 
-                                                       const VkSurfaceKHR& surface);
-
+/**
+* @brief Get collection of presentation modes of a swap chain created from physical device.
+*/
 [[nodiscard]]
 std::optional<VkSurfaceFormatKHR>
 find_swap_chain_surface_format(const std::vector<VkSurfaceFormatKHR>& surfaceformats,
@@ -237,12 +358,5 @@ VkDevice get_logical_device(const VkPhysicalDevice physical_device,
 VkInstance create_instance(SDL_Window* window,
                            const ValidationLayers& validation_layers);
 
-
-[[nodiscard]]
-CreatedSwapChain create_swap_chain(const VkPhysicalDevice physical_device,
-                                   const VkDevice logical_device,
-                                   const VkSurfaceKHR window_surface,
-                                   const uint32_t width,
-                                   const uint32_t height);
 
 }
--- arc/Device.hpp
+++ arc/Device.hpp
@@ -1,10 +1,13 @@
 #pragma once
+/** *******************************************************************
+ * @file Device.hpp
+ * @brief The Device Group needed to do anything in Vulkan.
+ * @copyright
+ * Copyright 2024 Thomas Alexgaard Jensen
+ *********************************************************************/
 
-#include "SDLVulkan.hpp"
-
-#include "TypeTraits.hpp"
-#include "Algorithm.hpp"
 #include "GlobalContext.hpp"
+#include "Algorithm.hpp"
 
 #include <vector>
 #include <optional>
@@ -12,7 +15,7 @@
 namespace ArcGraphics {
 
 /**
- * @brief Devices & Context object.
+ * @brief Devices & Vulkan Instance Manager.
  * A small collection of global objects, used almost everywhere in the renderer.
  * @todo restructure this class so that it is more explicitly called Devices,
  * and access is then just logical() or physical().
@@ -22,34 +25,55 @@ namespace ArcGraphics {
 class Device : private IsNotLvalueCopyable
 {
 public:
+
     class Builder;
 
+    /**
+     * @brief Destroy the devices and vulkan instance.
+     */
+    void destroy();
+
+    ~Device() = default;
+
+    /**
+     * @brief Get the Vulkan Instance.
+     */
+    [[nodiscard]]
+    const VkInstance& instance() const noexcept;
+
+    /**
+     * @brief Get the physical device.
+     */
+    [[nodiscard]]
+    const VkPhysicalDevice& physical_device() const noexcept;
+
+    /**
+     * @brief Get the logical device.
+     */
+    [[nodiscard]]
+    const VkDevice& logical_device() const noexcept;
+
+    /**
+     * @brief Get the rendering capabilities of the physical device.
+     */
+    [[nodiscard]]
+    const DeviceRenderingCapabilities& capabilities() const noexcept;
+
+private:
+     /**
+     * @brief Construct the Devices.
+     * @note is internally handled by the builder.
+     * @see ArcGraphics::Device::Builder
+     */
     Device(const VkInstance instance,
            const VkPhysicalDevice physical_device,
            const VkDevice logical_device,
            const DeviceRenderingCapabilities capabilities);
 
-    void destroy();
-
-    ~Device() = default;
-
-    [[nodiscard]]
-    const VkInstance& instance() const noexcept;
-
-    [[nodiscard]]
-    const VkPhysicalDevice& physical_device() const noexcept;
-
-    [[nodiscard]]
-    const VkDevice& logical_device() const noexcept;
-
-    [[nodiscard]]
-    const DeviceRenderingCapabilities& capabilities() const noexcept;
-
-private:
-    VkInstance m_instance;
-    VkPhysicalDevice m_physical_device;
-    VkDevice m_logical_device;
-    DeviceRenderingCapabilities m_capabilities;
+    VkInstance m_instance;                      /// Vulkan instance
+    VkPhysicalDevice m_physical_device;         /// physical device
+    VkDevice m_logical_device;                  /// logical device
+    DeviceRenderingCapabilities m_capabilities; /// rendering capabilities
 };
     
 /**
@@ -65,16 +89,31 @@ private:
 class Device::Builder : protected IsNotLvalueCopyable
 {
 public:
+
+    /**
+     * @brief Builder Constructor ensures that the global context is initialized.
+     * @see ArcGraphics::GlobalContext::Initialize
+     */
     Builder();
     ~Builder() = default;
 
+    /**
+     * @brief Add a validation layer to the logical device for debugging.
+     */
     Builder& add_validation_layers(const ValidationLayers layers);
+
+    /**
+     * @brief Add the khronos validation layer to the logical device for debugging.
+     */
     Builder& add_khronos_validation_layer();
 
+    /**
+     * @brief Produce the Device.
+     */
     Device produce();
     
 private:
-    ValidationLayers m_validation_layers{};
+    ValidationLayers m_validation_layers{}; /// The validation layers to be enabled
 };
  
 
